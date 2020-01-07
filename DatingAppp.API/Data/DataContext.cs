05c35a5aab3ffbe7ee06e5ff021b6819416ac47c
Anie Edem <anieedem@Anies-MacBook-Air.local>
2020-01-07 12:36:07 +0200

    initial Commit

---
+++ DatingAppp.API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using DatingAppp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppp.API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Value> Values { get; set; }     
+    }
+}
