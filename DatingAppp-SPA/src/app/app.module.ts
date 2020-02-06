e61b3cd9c266dd723cc1e739337a4a45b4089046
Anie Edem <anieedem@Anies-MacBook-Air.local>
2020-02-06 12:28:47 +0200

    End of Section Thirteen

--- DatingAppp-SPA/src/app/app.module.ts
+++ DatingAppp-SPA/src/app/app.module.ts
@@ -27,6 +27,7 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/member-list/photo-editor/photo-editor.component';
 import { FileUploadModule } from 'ng2-file-upload';
+import {TimeAgoPipe} from 'time-ago-pipe';
 
 
 
@@ -53,7 +54,9 @@ export class CustomHammerConfig extends HammerGestureConfig {
       MemberCardComponent,
       MemberDetailComponent,
       MemberEditComponent,
-      PhotoEditorComponent
+      PhotoEditorComponent,
+      TimeAgoPipe
+
    ],
    imports: [
       BrowserModule,
